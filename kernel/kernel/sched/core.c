45073213ce61d9a124671bc01b6c17f40f381053
jangsutsr <jangsutsr@gmail.com>
2016-10-30 22:34:39 -0400

    Set sched_wrr_class as default task sched_class.

--- kernel/kernel/sched/core.c
+++ kernel/kernel/sched/core.c
@@ -1705,7 +1705,7 @@ void sched_fork(struct task_struct *p)
 	}
 
 	if (!rt_prio(p->prio))
-		p->sched_class = &fair_sched_class;
+		p->sched_class = &sched_wrr_class;
 
 	if (p->sched_class->task_fork)
 		p->sched_class->task_fork(p);
